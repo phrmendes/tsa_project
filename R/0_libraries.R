31ffdaf5a6b5a5f051a8ce5fcdc24f9825375d40
Pedro Mendes <phrmendes@tuta.io>
2022-10-19 18:33:25 -0300

    Att. renv.

--- R/0_libraries.R
+++ R/0_libraries.R
@@ -2,9 +2,16 @@
 # --- LIBRARIES --- #
 # ----------------- #
 
+options(repos = c(RSPM = "https://packagemanager.rstudio.com/all/latest"))
+
 if (!require("pacman")) install.packages("pacman")
 
-options(repos = c(RSPM = "https://packagemanager.rstudio.com/all/latest"))
+if (!require("stringi")) install.packages(
+  "stringi", 
+  dependencies = TRUE, 
+  INSTALL_opts = '--no-lock', 
+  configure.args='--disable-pkg-config'
+)
 
 pacman::p_load(
   languageserver,
